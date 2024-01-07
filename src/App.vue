28c8f3823596a0749925daa990f017d577d2e6c9
Syde2 <contact@syde2hub.com>
2024-01-07 18:12:27 +0100

    POC Hour OK

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,10 @@
 <script setup>
+import {computed, ref} from 'vue';
+
+const heure = getCurrentHour()
+const angle = computed(()=>
+  convertirHeureEnAngle(heure)
+)
 
 function convertirHeureEnAngle(heure) {
     // Définir les points de données
@@ -15,11 +21,20 @@ function interpolationLineaire(x, x1, y1, x2, y2) {
     return y1 + (x - x1) * (y2 - y1) / (x2 - x1);
 }
 
+function getCurrentHour(){
+  const time = new Date();
+  const hours = time.getHours() % 12 || 12;
+  return(hours)
+}
+
+
 </script>
 
 <template>
   <h1>Progressive Watch </h1>
   <div class="watch">
+    ANGLE :  {{ angle }}
+    HEURE : {{ heure }}
 
   </div>
 
@@ -27,5 +42,25 @@ function interpolationLineaire(x, x1, y1, x2, y2) {
 
 <style>
 
+:root{
+	--lighthouse: #F3F4F4;
+	--porcelainJasper : #DFE2E4;
+	--pavedGold:#E8D284;
+	--canadianMaple:#CAB266;
+	--shadowNight : #2A4F61;
+	--petrolSlumber : #243640;
+}
+.watch{
+	height: 300px;
+	width: 300px;
+	border-radius: 50%;
+	background-image: conic-gradient(
+		from  v-bind(angle+'deg'),
+		var(--pavedGold), 
+		var(--shadowNight)
+		);
+
+  }
+  
 
 </style>
