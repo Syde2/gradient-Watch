2e5f7a127782e29a56ab6f06822ef7abbb3f5f80
Syde2 <contact@syde2hub.com>
2024-01-07 17:14:46 +0100

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+# gradient Watch
+
+
+## Intention 
+Puis-je reproduire le modele de cette montre analogique dont les aiguilles sont représentée par une marque de gradient ?  : 
+
+![Image](https://pbs.twimg.com/media/GDPvl9lWYAAx9lb?format=jpg&name=medium)
+
+
+
+----
+
+
+#### MindfulPalettes series #31  : [lien](https://twitter.com/AlexCristache/status/1743542925219573846)
+![palette](https://pbs.twimg.com/media/GDGhsqxXQAAWkOD?format=jpg&name=4096x4096)
+
+```css
+:root{
+	--lighthouse: #F3F4F4;
+	--porcelainJasper : #DFE2E4;
+	--pavedGold:#E8D284;
+	--canadianMaple:#CAB266;
+	--shadowNight : #2A4F61;
+	--petrolSlumber : #243640;
+}
+```
---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+
+function convertirHeureEnAngle(heure) {
+    // Définir les points de données
+    const pointsHeures = [0, 3, 6, 9, 12];
+    const pointsAngles = [0, 90, 180, 270, 360];
+
+    // Interpoler l'angle en fonction de l'heure
+    const angleInterpole = interpolationLineaire(heure, pointsHeures[0], pointsAngles[0], pointsHeures[pointsHeures.length - 1], pointsAngles[pointsAngles.length - 1]);
+
+    return angleInterpole;
+}
+// Fonction d'interpolation linéaire
+function interpolationLineaire(x, x1, y1, x2, y2) {
+    return y1 + (x - x1) * (y2 - y1) / (x2 - x1);
+}
+
+</script>
+
+<template>
+  <h1>Progressive Watch </h1>
+  <div class="watch">
+
+  </div>
+
+</template>
+
+<style>
+
+
+</style>
